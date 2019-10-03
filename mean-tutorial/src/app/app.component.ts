7262a8f9dd5c94981e27921846aa74b19e1250da
ryanwardle <wardle2106@gmail.com>
2019-10-03 15:26:24 -0400

    initial commit

---
+++ mean-tutorial/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'mean-tutorial';
+}
