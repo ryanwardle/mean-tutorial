5932be9e4b39fc45344d70782d1113975f4a5839
ryanwardle <wardle2106@gmail.com>
2019-10-04 16:31:06 -0400

    update

--- mean-tutorial/src/app/posts/post-create/post-create.component.ts
+++ mean-tutorial/src/app/posts/post-create/post-create.component.ts
@@ -2,10 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
-  templateUrl: './post-create.component.html'
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
 })
 
 export class PostCreateComponent {
 
+  enteredValue = '';
+  newPost = 'NO CONTENT';
+
+  onAddPost(postInput: HTMLTextAreaElement) {
+    this.newPost = this.enteredValue;
+  }
 }
 
