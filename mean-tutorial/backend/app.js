36503d213d1a2fcdeedaa56bb9a9f177e6141aed
ryanwardle <wardle2106@gmail.com>
2019-10-12 14:59:56 -0400

    beginning to add mongoDB

--- mean-tutorial/backend/app.js
+++ mean-tutorial/backend/app.js
@@ -1,15 +1,30 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.json());
+
+// Don't need for this project, but exists
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With', 'Content-Type', 'Accept');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS')
   next();
 });
 
-app.use('/api/posts', (req, res, next) => {
+
+
+app.post('/api/posts', (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added successfully'
+  });
+});
+
+app.get('/api/posts', (req, res, next) => {
   const posts = [
     {id: 'fanss9458h849v',
     title: 'First server side post',
