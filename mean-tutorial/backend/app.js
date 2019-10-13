0a6bb39469b44e695a1a5d73f11153ae69f5f6eb
ryanwardle <wardle2106@gmail.com>
2019-10-13 15:11:53 -0400

    connected to database added delete

--- mean-tutorial/backend/app.js
+++ mean-tutorial/backend/app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect('mongodb+srv://ryan:tG4K2iRg4u7lMKcb@mean-tutorial-8x7sf.mongodb.net/node-angular?retryWrites=true&w=majority').then(() => {
+  console.log('Connected to database')
+}).catch(() => {
+  console.log('Connection failed')
+});
+
+const Post = require('./models/post');
 
 app.use(bodyParser.json());
 
 // Don't need for this project, but exists
-app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.urlencoded({extended: false}));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -14,29 +23,35 @@ app.use((req, res, next) => {
   next();
 });
 
-
-
 app.post('/api/posts', (req, res, next) => {
-  const post = req.body;
-  console.log(post);
-  res.status(201).json({
-    message: 'Post added successfully'
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: 'Post added successfully',
+      postId: createdPost._id
+    });
   });
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {id: 'fanss9458h849v',
-    title: 'First server side post',
-    content: 'This content is coming from the server'},
-    {id: 'sdjnfw45349im',
-     title: 'This is the second post',
-    content: 'This is also coming from the server'}
-  ];
-  res.status(200).json({
-    message: 'Posts fetched successfully',
-    posts: posts
+  Post.find().then((documents) => {
+    res.status(200).json({
+      message: 'Posts fetched successfully',
+      posts: documents
+    });
   });
 });
 
+app.delete('/api/posts/:id', (req, res, next) => {
+  Post.deleteOne({_id: req.params.id}).then(result => {
+    console.log(result);
+    res.status(200).json({message: 'Post deleted!'});
+  })
+})
+
 module.exports = app;
+
+
--- mean-tutorial/src/app/posts/posts.service.ts
+++ mean-tutorial/src/app/posts/posts.service.ts
@@ -2,6 +2,7 @@ import { Post } from './post.model';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
@@ -11,8 +12,16 @@ export class PostsService {
   constructor(private http: HttpClient) {}
 
   getPosts() {
-    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts').subscribe((postData) => {
-      this.posts = postData.posts;
+    this.http.get<{message: string, posts: any}>('http://localhost:3000/api/posts').pipe(map((postData) => {
+      return postData.posts.map(posts => {
+        return {
+          title: posts.title,
+          content: posts.content,
+          id: posts._id
+        };
+      });
+    })).subscribe(transformedPosts => {
+      this.posts = transformedPosts;
       this.postsUpdated.next([...this.posts]);
     });
   }
@@ -23,9 +32,18 @@ export class PostsService {
 
   addPost(title: string, content: string) {
     const post: Post = {id: null, title, content};
-    this.http.post<{message: string}>('http://localhost:3000/api/posts', post).subscribe((responseData) => {
-      console.log(responseData.message);
+    this.http.post<{message: string, postId: string}>('http://localhost:3000/api/posts', post).subscribe((responseData) => {
+      const id = responseData.postId;
+      post.id = id;
       this.posts.push(post);
+      this.postsUpdated.next([...this.posts]);
+    });
+  }
+
+  deletePost(postId: string) {
+    this.http.delete('http://localhost:3000/api/posts/' + postId).subscribe(() => {
+      const updatedPosts = this.posts.filter(post => post.id !== postId);
+      this.posts = updatedPosts;
       this.postsUpdated.next([...this.posts]);
     });
   }
