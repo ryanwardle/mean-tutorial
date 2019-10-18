e778cb34c0e72fe3235d310c36072d5628cc7420
ryanwardle <wardle2106@gmail.com>
2019-10-18 15:40:55 -0400

    selecting image file

--- mean-tutorial/src/app/posts/post-create/post-create.component.ts
+++ mean-tutorial/src/app/posts/post-create/post-create.component.ts
@@ -15,6 +15,7 @@ export class PostCreateComponent implements OnInit {
   enteredContent = '';
   enteredTitle = '';
   post: Post;
+  imagePreview: string;
   isLoading = false;
   form: FormGroup;
   private mode = 'create';
@@ -26,7 +27,8 @@ export class PostCreateComponent implements OnInit {
   ngOnInit() {
     this.form = new FormGroup({
       title: new FormControl(null, {validators: [Validators.required, Validators.minLength(3)]}),
-      content: new FormControl(null, {validators: [Validators.required]})
+      content: new FormControl(null, {validators: [Validators.required]}),
+      image: new FormControl(null, {validators: Validators.required})
     });
 
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
@@ -51,6 +53,17 @@ export class PostCreateComponent implements OnInit {
         this.postId = null;
       }
     });
+  }
+
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({image: file});
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result as string;
+    };
+    reader.readAsDataURL(file);
   }
 
   onSavePost() {
