fd5f7294e4c264aa0735a672c048ce8fee8c47d2
ryanwardle <wardle2106@gmail.com>
2019-10-20 16:07:40 -0400

    working on image upload

--- mean-tutorial/src/app/posts/post-create/post-create.component.ts
+++ mean-tutorial/src/app/posts/post-create/post-create.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { PostsService } from '../posts.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Post } from '../post.model';
+import { mimeType } from './mime-type.validators';
 
 @Component({
   selector: 'app-post-create',
@@ -28,7 +29,7 @@ export class PostCreateComponent implements OnInit {
     this.form = new FormGroup({
       title: new FormControl(null, {validators: [Validators.required, Validators.minLength(3)]}),
       content: new FormControl(null, {validators: [Validators.required]}),
-      image: new FormControl(null, {validators: Validators.required})
+      image: new FormControl(null, {validators: [Validators.required], asyncValidators: [mimeType]})
     });
 
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
@@ -41,11 +42,12 @@ export class PostCreateComponent implements OnInit {
           this.post = {
             id: postData._id,
             title: postData.title,
-            content: postData.content
+            content: postData.content,
+            imagePath: null
           };
           this.form.setValue({
             title: this.post.title,
-            content: this.post.content
+            content: this.post.content,
           });
         });
       } else {
@@ -70,7 +72,7 @@ export class PostCreateComponent implements OnInit {
     if (this.form.invalid) { return; }
     this.isLoading = true;
     if (this.mode === 'create') {
-      this.postsService.addPost(this.form.value.title, this.form.value.content);
+      this.postsService.addPost(this.form.value.title, this.form.value.content, this.form.value.image);
     } else {
       this.postsService.updatePost(this.postId, this.form.value.title, this.form.value.content);
     }
--- mean-tutorial/src/app/posts/posts.service.ts
+++ mean-tutorial/src/app/posts/posts.service.ts
@@ -19,7 +19,8 @@ export class PostsService {
         return {
           title: post.title,
           content: post.content,
-          id: post._id
+          id: post._id,
+          imagePath: post.imagePath
         };
       });
     })).subscribe(transformedPosts => {
@@ -36,11 +37,19 @@ export class PostsService {
     return this.http.get<{_id: string, title: string, content: string}>('http://localhost:3000/api/posts/' + id);
   }
 
-  addPost(title: string, content: string) {
-    const post: Post = {id: null, title, content};
-    this.http.post<{message: string, postId: string}>('http://localhost:3000/api/posts', post).subscribe((responseData) => {
-      const id = responseData.postId;
-      post.id = id;
+  addPost(title: string, content: string, image: File) {
+    const postData = new FormData();
+    postData.append('title', title);
+    postData.append('content', content);
+    postData.append('image', image, title);
+
+    this.http.post<{message: string, post: Post}>('http://localhost:3000/api/posts', postData).subscribe((responseData) => {
+      const post: Post = {
+        id: responseData.post.id,
+        title,
+        content,
+        imagePath: responseData.post.imagePath
+      };
       this.posts.push(post);
       this.postsUpdated.next([...this.posts]);
     });
@@ -51,7 +60,8 @@ export class PostsService {
     const post: Post  = {
       id,
       title,
-      content
+      content,
+      imagePath: null
     };
     this.http.put('http://localhost:3000/api/posts/' + id, post).subscribe((response) => {
       const updatedPosts = [...this.posts];
