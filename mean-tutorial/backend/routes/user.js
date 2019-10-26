0749e5fe54a7e722cb74c2519f28b00da111549b
ryanwardle <wardle2106@gmail.com>
2019-10-26 16:33:34 -0400

    work on user signup

--- mean-tutorial/backend/routes/user.js
+++ mean-tutorial/backend/routes/user.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
 const router = express.Router();
 
 router.post('/signup', (req, res, next) => {
-
+  // Encrypts passwords
+  bcrypt.hash(req.body.password, 10).then(hash => {
+    const user = new User({
+      email: req.body.email,
+      password: hash
+    });
+    user.save().then(result => {
+      result.status(201).json({
+        message: 'User created!',
+        result: result
+      });
+    });
+  });
 });
 
 module.exports = router;
